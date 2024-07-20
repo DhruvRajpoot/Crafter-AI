3976eacf5fb4f54e2f954a1000a738113ba4a5e2
Dhruv Rajpoot <dhruv.kutarya@gmail.com>
2024-07-20 21:35:18 +0530

    installed shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import React from "react";
 
 const page = () => {
   return (
-    <div className='text-red-600'>page</div>
-  )
-}
+    <>
+      <div className="text-red-600">page</div>
+      <Button>ShadCN Button</Button>
+    </>
+  );
+};
 
-export default page
+export default page;
