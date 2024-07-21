5b54cce83752e80e4c7c1f963cba6307e4567d00
Dhruv Rajpoot <dhruv.kutarya@gmail.com>
2024-07-22 01:44:31 +0530

    feat: signup signin & logout button

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
 
 const DashboardPage = () => {
   return (
-    <div>DashboardPage</div>
-  )
-}
+    <div>
+      DashboardPage
+      <div>
+        <UserButton afterSwitchSessionUrl="/sign" />
+      </div>
+    </div>
+  );
+};
 
-export default DashboardPage
+export default DashboardPage;
