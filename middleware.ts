0f701d52d965749519307287e517d5b0d2dd4d15
Dhruv Rajpoot <dhruv.kutarya@gmail.com>
2024-08-11 02:35:27 +0530

    create user in db using clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -15,5 +15,10 @@ export default clerkMiddleware((auth, req) => {
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!.*\\..*|_next).*)",
+    "/",
+    "/(api|trpc)(.*)",
+    "/api/webhooks/(.*)",
+  ],
 };
